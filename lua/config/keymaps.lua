55f81a4d454776bf228d566dbf8360ea62a2ea07
Stefan Partheymüller <s.partheymueller@initics.de>
2023-07-30 18:46:56 +0200

    cleanup keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local Util = require("lazyvim.util")
-local MiniComment = require("mini.comment")
 local map = vim.keymap.set
 local del = vim.api.nvim_del_keymap
 
--- Unset keymaps
+-- Unset unwanted keymaps
 del("n", "<C-/>")
 del("n", "<leader>xl")
 del("n", "<leader>xq")
@@ -16,21 +10,27 @@ del("t", "<C-j>")
 del("t", "<C-k>")
 del("t", "<C-l>")
 
--- Normal mode
-if Util.has("bufferline.nvim") then
-  map("n", "<S-tab>", "<Cmd>BufferLineCyclePrev<Cr>", { desc = "Prev buffer" })
-  map("n", "<tab>", "<Cmd>BufferLineCycleNext<Cr>", { desc = "Next buffer" })
-else
-  map("n", "<S-tab>", "<Cmd>bprevious<Cr>", { desc = "Prev buffer" })
-  map("n", "<tab>", "<Cmd>bnext<Cr>", { desc = "Next buffer" })
-end
-map("n", "<Esc>", "<Cmd>noh<Cr>", { desc = "Clear highlighing" })
-map("n", "<C-y>", "<Cmd>%y+<Cr>", { desc = "Copy whole file" })
-map("n", "<C-_>", function()
-  return MiniComment.operator() .. "_"
-end, { expr = true, desc = "Comment line" })
+--
+-- General
+--
+map("n", "<C-y>", "<Cmd>%y+<Cr>", { desc = "Copy current buffer to system clipboard" })
 
--- Insert mode
+--
+-- Buffers
+--
+map("n", "<S-Tab>", "<S-h>", { remap = true, desc = "Previous buffer" })
+map("n", "<Tab>", "<S-l>", { remap = true, desc = "Next buffer" })
+map("n", "<leader>x", "<leader>bd", { remap = true, desc = "Close current buffer" })
+
+--
+-- Coding
+--
+-- Toggle line comment
+map("n", "<C-_>", "gcc", { remap = true, desc = "Toggle line comment" })
+-- Toggle comment (visual mode)
+map("v", "<C-_>", "gc", { remap = true, desc = "Comment selection" })
+
+-- Cursor navigation (insert mode)
 map("i", "<C-h>", "<Left>", { desc = "Move cursor left" })
 map("i", "<C-l>", "<Right>", { desc = "Move cursor right" })
 map("i", "<C-j>", "<Down>", { desc = "Move cursor down" })
@@ -38,5 +38,5 @@ map("i", "<C-k>", "<Up>", { desc = "Move cursor up" })
 map("i", "<C-b>", "<Esc>^i", { desc = "Move cursor to beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "Move cursor to end of line" })
 
--- Terminal mode
+-- Terminal
 map("t", "<C-x>", vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), { desc = "Enter Normal Mode" })
--- lua/plugins/mini-comment.lua
+++ lua/plugins/mini-comment.lua
@@ -5,12 +5,6 @@ return {
       options = {
         ignore_blank_lines = true,
       },
-      mappings = {
-        -- Normal and Visual modes
-        comment = "<C-_>",
-        -- Toggle comment on current line
-        comment_line = "<C-_>",
-      },
     },
   },
 }
