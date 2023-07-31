3a03087ec00ff1849c5667368adfd6bd9f325e31
Stefan Partheymüller <s.partheymueller@initics.de>
2023-07-31 11:02:10 +0200

    customize dashboard header logo

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,4 +7,20 @@ return {
       },
     },
   },
+  {
+    "goolord/alpha-nvim",
+    dependencies = {
+      {
+        "MaximilianLloyd/ascii.nvim",
+        dependencies = {
+          "MunifTanjim/nui.nvim",
+        },
+      },
+    },
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local neovim = require("ascii.text.neovim")
+      dashboard.section.header.val = neovim.sharp
+    end,
+  },
 }
