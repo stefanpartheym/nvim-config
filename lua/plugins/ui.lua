e4157e712d54c3e7c4bd58960ecf848cbd92ed9b
Stefan Partheymüller <s.partheymueller@initics.de>
2023-10-27 11:58:57 +0200

    switch from alpha.nvim to dashboard-nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
     },
   },
   {
-    "goolord/alpha-nvim",
+    "nvimdev/dashboard-nvim",
     dependencies = {
       {
         "MaximilianLloyd/ascii.nvim",
@@ -16,11 +16,34 @@ return {
           "MunifTanjim/nui.nvim",
         },
       },
+      { "nvim-tree/nvim-web-devicons" },
     },
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local neovim = require("ascii.text.neovim")
-      dashboard.section.header.val = neovim.sharp
+    event = "VimEnter",
+    config = function()
+      local logo = require("ascii.text.neovim")
+      require("dashboard").setup({
+        theme = "doom",
+        config = {
+          header = logo.sharp,
+          -- stylua: ignore
+          center = {
+            { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+            { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+            { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+            { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+            { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      })
     end,
   },
 }
