209eacb11f092270ce1a134dd3ae984d4b6df1fb
Stefan Partheymüller <s.partheymueller@initics.de>
2024-01-12 12:05:31 +0100

    swap keymaps for neo-tree (root dir) and neo-tree (cwd)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,6 +4,10 @@ local MiniBufremove = require("mini.bufremove")
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+      { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    },
     opts = {
       filesystem = {
         filtered_items = {
