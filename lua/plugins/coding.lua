05d5febe4fd2ad3114287f67a5d42fc5d435ffbd
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-11 19:16:16 +0200

    add coding plugins: mini.pairs, mini.ai, ts-comments.nvim

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,35 @@
+return {
+  -- Auto pairs
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
+    },
+  },
+
+  -- Better text-objects
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  -- Comments
+  {
+    "folke/ts-comments.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
