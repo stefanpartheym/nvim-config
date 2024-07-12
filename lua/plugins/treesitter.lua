ca7d2217c976786e136d10edb4d6cd23b0b6aa3e
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-12 15:48:23 +0200

    fix nvim-treesitter installation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,31 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-      -- Autoinstall languages that are not installed
+      -- Do not preinstall any parsers.
+      ensure_installed = {},
+      -- Automatically install missing parsers when entering buffer.
       auto_install = true,
-      highlight = {
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      -- Enable incremental selection
+      incremental_selection = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
       },
-      indent = { enable = true, disable = { "ruby" } },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       -- Prefer git instead of curl in order to improve connectivity in some environments
       require("nvim-treesitter.install").prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup(opts)
 
       -- There are additional nvim-treesitter modules that you can use to interact
