6bdc3d415c293ef8bd80f66f3ee793a9a008c056
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-13 23:37:29 +0200

    telescope: refactor config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -255,27 +255,23 @@ return {
       -- Enable telescope extensions, if they are installed
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
-
-      -- See `:help telescope.builtin`
+    end,
+    keys = function()
       local builtin = require("telescope.builtin")
-
-      vim.keymap.set(
-        "n",
-        "<leader>,",
-        "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
-        { desc = "Switch Buffer" }
-      )
-      vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search help" })
-      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search keymaps" })
-      vim.keymap.set("n", "<leader>sm", builtin.marks, { desc = "Search marks" })
-      vim.keymap.set("n", "<leader>sM", builtin.man_pages, { desc = "Search man pages" })
-      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search files" })
-      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Grep search" })
-      vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Search current word" })
-      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search diagnostics" })
-      vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Resume search" })
-      vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "Search recent files" })
-      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Search existing buffers" })
+      return {
+        { "<leader>,", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffer" },
+        { "<leader>sh", builtin.help_tags, desc = "Search help" },
+        { "<leader>sk", builtin.keymaps, desc = "Search keymaps" },
+        { "<leader>sm", builtin.marks, desc = "Search marks" },
+        { "<leader>sM", builtin.man_pages, desc = "Search man pages" },
+        { "<leader>sf", builtin.find_files, desc = "Search files" },
+        { "<leader>sg", builtin.live_grep, desc = "Grep search" },
+        { "<leader>sw", builtin.grep_string, desc = "Search current word" },
+        { "<leader>sd", builtin.diagnostics, desc = "Search diagnostics" },
+        { "<leader>sr", builtin.resume, desc = "Resume search" },
+        { "<leader>s.", builtin.oldfiles, desc = "Search recent files" },
+        { "<leader><leader>", builtin.buffers, desc = "Search existing buffers" },
+      }
     end,
   },
 }
