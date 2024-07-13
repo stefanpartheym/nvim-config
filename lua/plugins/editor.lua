6f72b7e73abb59e379e674276854140b2bbd3409
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-14 00:16:06 +0200

    gitsigns: refactor config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -31,28 +31,24 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
-    opts = {
-      signs = {
+    opts = function()
+      local signs = {
         add = { text = "▎" },
         change = { text = "▎" },
         delete = { text = "" },
         topdelete = { text = "" },
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
-      },
-      signs_staged = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "▎" },
-      },
-      on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
+      }
+      return {
+        signs = signs,
+        signs_staged = signs,
+        on_attach = function(buffer)
+          local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-        end
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
 
         -- stylua: ignore start
         map("n", "]h", function()
@@ -82,8 +78,9 @@ return {
         map("n", "<leader>ghd", gs.diffthis, "Diff This")
         map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-      end,
-    },
+        end,
+      }
+    end,
   },
 
   -- Better diagnostics list and others
