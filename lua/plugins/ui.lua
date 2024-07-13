627299bd3e678ccebd6672f65a779503a5c8b08c
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-13 16:24:30 +0200

    add plugin persistence.nvim to save and load session state

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -222,7 +222,7 @@ return {
             { action = "ene | startinsert",                 desc = " New file",        icon = " ", key = "n" },
             { action = "Telescope oldfiles",                desc = " Recent files",    icon = " ", key = "r" },
             { action = "Telescope live_grep",               desc = " Find text",       icon = " ", key = "g" },
-            -- { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
             { action = "Lazy",                              desc = " Lazy",            icon = "󰒲 ", key = "l" },
             { action = "qa",                                desc = " Quit",            icon = " ", key = "q" },
           },
