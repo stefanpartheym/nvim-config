0774c5b52df11a33109ac19e831280b3434fcfc0
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-14 12:53:28 +0200

    bufferline: change event to VimEnter to make sure the plugin is loaded, add additional keymaps

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,7 @@
 return {
   {
     "akinsho/bufferline.nvim",
+    event = "VimEnter",
     opts = {
       options = {
         always_show_bufferline = true,
@@ -19,6 +20,13 @@ return {
           },
         },
       },
+    },
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
     },
   },
 
