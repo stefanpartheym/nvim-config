804fd4eaa09884b1b485bb5fe1287e404ddf71c9
Stefan Partheymüller <s.partheymueller@initics.de>
2024-07-14 11:03:33 +0200

    bufferline remove heading text in left offset when neo-tree is open

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,7 +13,7 @@ return {
         offsets = {
           {
             filetype = "neo-tree",
-            text = "Neo-tree",
+            text = "",
             highlight = "Directory",
             text_align = "left",
           },
