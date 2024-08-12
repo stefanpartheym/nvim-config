458c7559a2b41b77477cf06aedf8b8a5ea4d0453
Stefan Partheymüller <s.partheymueller@initics.de>
2024-08-12 14:41:39 +0400

    add formatter definition for zig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -322,12 +322,20 @@ return {
           xml = { "xmlformat" },
           php = { "pretty-php" },
           twig = { "djlint" },
+          zig = { "zigfmt" },
           -- Conform can also run multiple formatters sequentially
           -- python = { "isort", "black" },
           --
           -- You can use a sub-list to tell conform to run *until* a formatter
           -- is found.
           -- javascript = { { "prettierd", "prettier" } },
+        },
+        formatters = {
+          zigfmt = {
+            command = "zig",
+            args = { "fmt", "--stdin" },
+            stdin = true,
+          },
         },
       }
 
