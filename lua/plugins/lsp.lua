9f6025cb6620096dfb190e42e89d5a5fd4090e9f
Stefan Partheymüller <s.partheymueller@initics.de>
2024-11-12 21:15:35 +0100

    lspconfig: refactoring

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,7 +36,6 @@ return {
       "WhoIsSethDaniel/mason-tool-installer.nvim",
 
       -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { "j-hui/fidget.nvim", opts = {} },
 
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -44,31 +43,6 @@ return {
       { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -183,33 +157,35 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      --- List of manually managed servers (that is, servers not managed by Mason).
+      --- @type table<string, lspconfig.Config>
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        -- NOTE:
+        -- Setup `zls` manually to always use current version from `.zigversion`.
+        -- See [ziege](https://github.com/photex/ziege) for more information.
+        zls = {},
+        -- NOTE:
+        -- Setup `kotlin_language_server` manually to use forked version from
+        -- [github.com/kotlin-community-tools](https://github.com/kotlin-community-tools/kotlin-language-server).
+        -- This fork contains fixes, that prevent the language server from raising
+        -- annoying internal errors.
+        kotlin_language_server = {
+          cmd = {
+            os.getenv("HOME") .. "/.lsp/kotlin-language-server/server/build/install/server/bin/kotlin-language-server",
+          },
+        },
+      }
 
+      -- Setup manually managed language servers.
+      for server_name, server in pairs(servers) do
+        server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+        require("lspconfig")[server_name].setup(server)
+      end
+
+      --- List of configs for language servers managed by Mason.
+      --- @type table<string, lspconfig.Config>
+      local mason_managed_servers = {
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
@@ -228,6 +204,7 @@ return {
           },
         },
 
+        -- TODO: Consider also using [typescript-tools.nvim](https://github.com/pmizio/typescript-tools.nvim)
         ts_ls = {
           init_options = {
             preferences = {
@@ -238,52 +215,41 @@ return {
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
+      -- Setup Mason.
       require("mason").setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        -- Language servers
-        "lua-language-server",
-        "bash-language-server",
-        "clangd",
-        -- NOTE:
-        -- Never install `zls` via Mason: It would not work with the local `zls`
-        -- managed by `ziege`.
-        -- "zls",
-        "json-lsp",
-        "yaml-language-server",
-        "lemminx", -- XML
-        "css-lsp",
-        "html-lsp",
-        "emmet-ls",
-        "typescript-language-server",
-        "vue-language-server",
-        "intelephense",
-
-        -- Formatters/linters/etc.
-        "stylua",
-        "shfmt",
-        "prettier",
-        "clang-format",
-        "xmlformatter",
-        "pretty-php",
-        "twigcs",
-        "djlint",
+      -- Ensure the following servers and tools are installed via Mason.
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          -- Language servers
+          "lua-language-server",
+          "bash-language-server",
+          "clangd",
+          "json-lsp",
+          "yaml-language-server",
+          "lemminx", -- XML language server
+          "css-lsp",
+          "html-lsp",
+          "emmet-ls",
+          "typescript-language-server",
+          "vue-language-server",
+          "intelephense",
+          -- Formatters/linters/etc.
+          "stylua",
+          "shfmt",
+          "prettier",
+          "clang-format",
+          "xmlformatter",
+          "pretty-php",
+          "twigcs",
+          "djlint",
+        },
       })
-      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
+      -- Setup language servers managed by Mason.
       require("mason-lspconfig").setup({
         handlers = {
           function(server_name)
-            local server = servers[server_name] or {}
+            local server = mason_managed_servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
@@ -291,23 +257,6 @@ return {
             require("lspconfig")[server_name].setup(server)
           end,
         },
-      })
-
-      -- NOTE:
-      -- Setup `zls` manually to always use current version from `ziege`.
-      require("lspconfig").zls.setup({
-        capabilities = capabilities,
-      })
-      -- NOTE:
-      -- Setup `kotlin_language_server ` manually to use forked version from
-      -- https://github.com/kotlin-community-tools/kotlin-language-server.
-      -- This fork contains fixes, that prevent the language server from raising
-      -- annoying internal errors.
-      require("lspconfig").kotlin_language_server.setup({
-        cmd = {
-          os.getenv("HOME") .. "/.lsp/kotlin-language-server/server/build/install/server/bin/kotlin-language-server",
-        },
-        capabilities = capabilities,
       })
     end,
   },
@@ -402,7 +351,8 @@ return {
     end,
   },
 
-  { -- Autocompletion
+  -- Autocompletion
+  {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
