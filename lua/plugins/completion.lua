5d1fa7aa57fc5d2ad4e8a23f52329b746d2d2864
Stefan Partheymüller <s.partheymueller@initics.de>
2024-12-20 17:17:50 +0100

    completion: change keymaps

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,7 +10,20 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
+
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-l>"] = { "snippet_forward", "fallback" },
+        ["<C-h>"] = { "snippet_backward", "fallback" },
+      },
 
       appearance = {
         use_nvim_cmp_as_default = true,
@@ -59,7 +72,7 @@ return {
         },
       },
 
-      signature = { enabled = true },
+      signature = { enabled = false },
 
       completion = {
         menu = {
