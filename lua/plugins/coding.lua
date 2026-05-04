fb6959c73b739ebc74b56c86496cbcd553ddc4a9
Stefan Partheymüller <s.partheymueller@parthec.de>
2026-05-04 20:58:14 +0200

    refactor: replace vim-textobj-entire/user with mini.ai custom textobject

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -21,12 +21,19 @@ return {
   {
     "echasnovski/mini.ai",
     event = "VeryLazy",
-    opts = {},
-  },
-
-  {
-    "kana/vim-textobj-entire",
-    dependencies = { "kana/vim-textobj-user" },
+    opts = {
+      custom_textobjects = {
+        -- Entire buffer
+        e = function()
+          local from = { line = 1, col = 1 }
+          local to = {
+            line = vim.fn.line("$"),
+            col = math.max(vim.fn.getline("$"):len(), 1),
+          }
+          return { from = from, to = to }
+        end,
+      },
+    },
   },
 
   -- Surround
