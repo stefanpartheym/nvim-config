90da44e45e151d613c260e0360385c613edb8086
Stefan Partheymüller <s.partheymueller@parthec.de>
2026-05-10 19:59:30 +0200

    fix(treesitter): enable superhtml highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,6 +20,7 @@ return {
 
         -- Webdev
         "html",
+        "superhtml",
         "css",
         "javascript",
         "typescript",
@@ -33,6 +34,31 @@ return {
       local ts = require("nvim-treesitter")
       ts.setup()
       ts.install(default_languages)
+
+      -- Map .shtml files to the superhtml filetype
+      vim.filetype.add({
+        extension = {
+          shtml = "superhtml",
+        },
+      })
+
+      -- Start treesitter highlighting/indent for installed parsers
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("treesitter_start", { clear = true }),
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+          if not lang then
+            return
+          end
+          local ok, has_parser = pcall(vim.treesitter.language.add, lang)
+          if not ok or not has_parser then
+            return
+          end
+          if pcall(vim.treesitter.start, args.buf, lang) then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
     end,
   },
 }
